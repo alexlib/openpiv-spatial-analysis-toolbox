d323d239011eef6b7dd19c4aadb8e8880e021a8d
Alex Liberzon <alex.liberzon@gmail.com>
2017-12-09 17:09:01 +0200

    opened a function lior_mat_to_spatialbox_mat.m

---
+++ addons/lior_mat_to_spatialbox_mat.m
@@ -0,0 +1,4 @@
+function out = lior_mat_to_spatialbox_mat(matfile)
+% Lior created a new type of mat files using his GUI of MatPIV
+% we need to convert it to our MAT file type of spatialbox
+% to create time analysis, etc. 
